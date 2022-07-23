1f99b770a817e13c775cbf7ff00e11fee7d1dd3d
shanacon <n107502530@office365.ncu.edu.tw>
2022-07-23 20:20:59 +0800

    initial commit

---
+++ RegistryTracker/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RegistryTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrintKeys("SOFTWARE\\7-Zip");
+            mylabel.Text = "Tracking";
+        }
+        static void PrintKeys(string rkey)
+        {
+            //Queue<string> queue = new Queue<string>();
+            //queue.Enqueue(rkey);
+            //BFSTraversal(ref queue);
+            NodeTree root = new NodeTree(rkey.Split('\\')[1], rkey);
+            root.Construct();
+            root.DFS();
+        }
+        static void BFSTraversal(ref Queue<string> queue)
+        {
+            while (queue.Count != 0)
+            {
+                string Node = queue.Dequeue();
+                MessageBox.Show(Node);
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey(Node); ;
+                string[] names = rk.GetSubKeyNames();
+                foreach (string s in names)
+                {
+                    queue.Enqueue(Node + "\\" + s);
+                }
+            }
+        }
+    }
+}
---
+++ RegistryTracker/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RegistryTracker
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    class NodeTree
+    {
+        private string name;
+        private string path;
+        private LinkedList<NodeTree> child;
+        public NodeTree(string name, string path)
+        {
+            this.name = name;
+            this.path = path;
+            child = new LinkedList<NodeTree>();
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public string getPath()
+        {
+            return path;
+        }
+        public LinkedList<NodeTree> getChild()
+        {
+            return child;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setPath(string path)
+        {
+            this.path = path;
+        }
+        public void AddChild(NodeTree node)
+        {
+            child.AddLast(node);
+        }
+        public void Construct()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey(path);
+            string[] names = rk.GetSubKeyNames();
+            foreach (string s in names)
+            {
+                child.AddLast(new NodeTree(s, path + "\\" + s));
+            }
+            foreach (NodeTree kid in child)
+            {
+                kid.Construct();
+            }
+        }
+        public void BFS()
+        {
+            Queue<NodeTree> queue = new Queue<NodeTree>();
+            MessageBox.Show(this.path);
+            foreach (NodeTree kid in child)
+            {
+                queue.Enqueue(kid);
+            }
+            while (queue.Count != 0)
+            {
+                queue.Dequeue()._BFS(ref queue);
+            }
+        }
+        public void _BFS(ref Queue<NodeTree> queue)
+        {
+            MessageBox.Show(this.path);
+            foreach (NodeTree kid in child)
+            {
+                queue.Enqueue(kid);
+            }
+            while (queue.Count != 0)
+            {
+                queue.Dequeue()._BFS(ref queue);
+            }
+        }
+        public void DFS()
+        {
+            MessageBox.Show(this.path);
+            foreach (NodeTree kid in child)
+            {
+                kid.DFS();
+            }
+        }
+    }
+}
